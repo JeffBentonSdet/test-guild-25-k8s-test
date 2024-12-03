c3e2329c11d7860a3e64825ea093a581c7976714
Jeff Benton <jbenton909@gmail.com>
2024-12-02 18:28:13 -0600

    clean up eol

--- calculator.test.js
+++ calculator.test.js
@@ -2,11 +2,11 @@ const Calculator = require("./calculator");
 
 describe('add', () => {
     test('basic add', () => {
-        expect(Calculator.add(5, 3)).toBe(8)
-    })
-})
+        expect(Calculator.add(5, 3)).toBe(8);
+    });
+});
 describe('subtract', () => {
     test('basic subtract', () => {
         expect(Calculator.subtract(10, 4)).toBe(6);
-    })
-})
+    });
+});
